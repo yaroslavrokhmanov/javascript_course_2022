da871a9de9941c2149a20e6d45e139cfd426f85c
Yaroslav <yaroslavrokhmanov@gmail.com>
2022-11-11 16:49:54 +0100

    finish

---
+++ lesson 159.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Lesson 159
+
+// №1
+// Сделайте функцию func, которая параметром будет принимать 3 числа и выводить в консоль их сумму. Пусть даны 3 переменные с числами:
+// let param1 = 1;
+// let param2 = 2;
+// let param3 = 3;
+
+function func(num1, num2, num3){
+    console.log(num1 + num2 + num3);
+  }
+  
+  let param1 = 1;
+  let param2 = 2;
+  let param3 = 3; 
+  
+  func(param1, param2, param3);
