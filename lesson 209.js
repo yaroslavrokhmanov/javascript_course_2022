31e6f1f923cbd7ebdc1465f0b5cd51c13678b6ba
Yaroslav <yaroslavrokhmanov@gmail.com>
2022-12-18 13:37:55 +0100

    finish

--- lesson 209.js
+++ lesson 209.js
@@ -1,12 +1,16 @@
 "use strict";
 
-// Деструктуризация массивов в JavaScript
+// Деструктуризация массива из функции в JavaScript
 // Функция, находящая сумму чисел
 // ====================================================
 // №1
 // В следующем коде части массива записываются в соответствующие переменные:
 
-// let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+// function func() {
+// 	return ['John', 'Smit', 'development', 'programmer', 2000];
+// }
+
+// let arr = func();
 
 // let name       = arr[0];
 // let surname    = arr[1];
@@ -14,13 +18,16 @@
 // let position   = arr[3];
 // let salary     = arr[4];
 // Переделайте этот код через деструктуризацию согласно изученной теории.
+// --------------------
 
-let arr = ['John', 'Smit', 'development', 'programmer', 2000];
-let [name, surname, department, positiv, salary] = arr;
+let [name, surname, department, positiv, salary] = func();
+
+function func() {
+	return ['John', 'Smit', 'development', 'programmer', 2000];
+}
+
 console.log(name);       // "John"
 console.log(surname);     // "Smit"
 console.log(department); // "development"
 console.log(positiv);    // "programmer"
-console.log(salary);     // 200
-
-
+console.log(salary);     // 2000
